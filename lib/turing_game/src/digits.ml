4ed3fab4630ca5db214da82eb2fefb5fdf4c10b8
Mathieu Barbin <mathieu.barbin@gmail.com>
2023-08-14 23:54:48 +0200

    remove dead code

--- lib/turing_game/src/digits.ml
+++
@@ -1,8 +0,0 @@
-open! Core
-
-type t = bool Digit.Tuple.t [@@deriving equal, compare, sexp_of]
-
-let empty = Digit.Tuple.return false
-let all = Digit.Tuple.return true
-let mem t digit = Digit.Tuple.get t digit
-let init ~f = Digit.Tuple.init ~f
